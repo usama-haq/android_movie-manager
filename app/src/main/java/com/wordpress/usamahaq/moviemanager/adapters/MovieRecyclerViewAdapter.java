720dbff15c4edd323f5441e577fdcd7dbf11e873
Usama Haq <usamahaq@outlook.com>
2017-08-10 00:50:13 +0500

    Creating movies lists using RecyclerView Part 3

---
+++ app/src/main/java/com/wordpress/usamahaq/moviemanager/adapters/MovieRecyclerViewAdapter.java
@@ -0,0 +1,89 @@
+package com.wordpress.usamahaq.moviemanager.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.wordpress.usamahaq.moviemanager.R;
+import com.wordpress.usamahaq.moviemanager.models.Movie;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by usama on 8/10/2017.
+ */
+
+public class MovieRecyclerViewAdapter extends RecyclerView.Adapter<MovieRecyclerViewAdapter.ViewHolder> {
+
+    List<Movie> movies;
+    Context context;
+
+    private Context getContext() {
+        return context;
+    }
+
+    public MovieRecyclerViewAdapter(Context context, List<Movie> movies) {
+        this.movies = movies;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Movie movie = movies.get(position);
+
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext())
+                .load(movie.getPosterPath())
+                .into(holder.ivMovieImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.ivMovieImage)
+        ImageView ivMovieImage;
+        @BindView(R.id.tvTitle)
+        TextView tvTitle;
+        @BindView(R.id.tvOVerview)
+        TextView tvOverview;
+        @BindView(R.id.cvMovie)
+        CardView cvMovie;
+
+        ViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+
+    }
+
+
+}
+
+
