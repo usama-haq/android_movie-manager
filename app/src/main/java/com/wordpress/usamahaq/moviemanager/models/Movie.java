d6d8f987e77ba0daf1510a46d8bef7687a53bc34
Usama Haq <usamahaq@outlook.com>
2017-08-10 00:28:56 +0500

    Creating movies lists using RecyclerView Part 1 & 2

---
+++ app/src/main/java/com/wordpress/usamahaq/moviemanager/models/Movie.java
@@ -0,0 +1,86 @@
+package com.wordpress.usamahaq.moviemanager.models;
+
+/**
+ * Created by usama on 8/10/2017.
+ */
+
+public class Movie {
+
+
+    String id;
+    String title;
+    String overview;
+    float voteAverage;
+    float voteCount;
+    String posterPath;
+    String backdropPath;
+
+    public Movie(String id, String title, String overview, float voteAverage, float voteCount, String posterPath, String backdropPath) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.voteAverage = voteAverage;
+        this.voteCount = voteCount;
+        this.posterPath = posterPath;
+        this.backdropPath = backdropPath;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public float getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(float voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public float getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(float voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+
+}
