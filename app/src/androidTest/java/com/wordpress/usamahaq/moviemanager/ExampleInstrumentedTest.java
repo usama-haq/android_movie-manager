c5d1587b5b4a4eac10665a55d60710472a247622
Usama Haq <usamahaq@outlook.com>
2017-08-11 13:32:52 +0500

    UI Testing with Espresso

--- app/src/androidTest/java/com/wordpress/usamahaq/moviemanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wordpress/usamahaq/moviemanager/ExampleInstrumentedTest.java
@@ -2,11 +2,19 @@ package com.wordpress.usamahaq.moviemanager;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.wordpress.usamahaq.moviemanager.activities.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +24,27 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(
+            MainActivity.class, true, true
+    ) {
+    };
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.wordpress.usamahaq.moviemanager", appContext.getPackageName());
+    }
+
+    @Test
+    public void validateRecyclerViewClickSecondElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+    }
+
+    @Test
+    public void validateRecyclerViewClickThirdElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
     }
 }
